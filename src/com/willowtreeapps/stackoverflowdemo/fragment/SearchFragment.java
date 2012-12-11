cf9e9e5dc54b306b1333f2ca6740c21c29524977
Derek Brameyer <derek.brameyer@gmail.com>
2012-12-11 11:27:33 -0500

    Stub adapter created.

--- src/com/willowtreeapps/stackoverflowdemo/fragment/SearchFragment.java
+++ src/com/willowtreeapps/stackoverflowdemo/fragment/SearchFragment.java
@@ -2,12 +2,14 @@ package com.willowtreeapps.stackoverflowdemo.fragment;
 
 import com.github.rtyley.android.sherlock.roboguice.fragment.RoboSherlockListFragment;
 import com.willowtreeapps.stackoverflowdemo.R;
+import com.willowtreeapps.stackoverflowdemo.model.Question;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -20,6 +22,7 @@ public class SearchFragment extends RoboSherlockListFragment {
     @InjectView(R.id.search_text) CancelEditText searchText;
 
     private int mPageNumber = 1;
+    private SearchListAdapter mSearchListAdapter;
 
     private String[] dataArray = new String[]{"This", "Is", "An", "Android", "App!"};
 
@@ -35,6 +38,40 @@ public class SearchFragment extends RoboSherlockListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.simple_list_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    private class SearchListAdapter extends BaseAdapter {
+
+        private Question.Response mResponse;
+
+        public SearchListAdapter(Question.Response response) {
+            mResponse = response;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return mResponse.items.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mResponse.items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
     }
 
     @Override
